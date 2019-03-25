38f3bc67b85c14fcc07c087dda452daab5dc7c79
Nataly Verdugo <natalyverdugo@sarcan.cl>
2019-03-25 11:58:23 -0300

    incrementador grafico

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharesModule } from '../shares/shares.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
-  declarations: [DashboardComponent, ProgressComponent, Graficas1Component, PagesComponent],
+  declarations: [DashboardComponent, ProgressComponent, Graficas1Component, PagesComponent, IncrementadorComponent, GraficoDonaComponent],
   exports: [DashboardComponent, ProgressComponent, Graficas1Component],
-  imports: [SharesModule, PAGES_ROUTES]
+  imports: [SharesModule, PAGES_ROUTES, FormsModule, ChartsModule, BrowserModule]
 })
 export class PagesModule {}
